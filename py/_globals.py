022e84e15b1b0d3e02f9fc89ab67d2d43d04e4ff
hayesall <hayesall@iu.edu>
2024-07-13 18:38:38 -0400

    ⚗️ Add a `_globals.py`
    ... with the intent that these get loaded in every time IPython loads.
    There might be some other way to do this but I'm not remembering it.

---
+++ py/_globals.py
@@ -0,0 +1,22 @@
+# Copyright © 2024 Alexander L. Hayes
+# MIT License
+
+from typing import Any
+
+from rich.box import MINIMAL_HEAVY_HEAD
+from rich.console import Console
+from rich.pretty import Pretty
+from rich.table import Table
+
+
+def tabprint(data: list[dict[str, Any]]):
+    if len(data) == 0:
+        print("Empty table")
+        return None
+
+    table = Table(*data[0].keys(), box=MINIMAL_HEAVY_HEAD)
+    for row in data:
+        table.add_row(*map(Pretty, row.values()))
+
+    console = Console()
+    console.print(table)
